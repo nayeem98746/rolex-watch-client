7487583d177bd1eec5a34c9bbd4daf4f63fe5e89
nayeem407 <sabbirahmednayeem941@gmail.com>
2021-12-26 12:07:47 +0600

    simple change

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,7 +6,7 @@ const About = () => {
         <Container>
         <Row>
           <Col><img src={img} alt="" /></Col>
-          <Col>There can be no watch house less in need of an introduction than Rolex. It is a brand whose name echoes beyond the boundaries of our industry. Synonymous with luxury, interchangeable with quality, imbued with a heritage that no amount of money, spin, or PR stunts can synthesize, Rolex almost occupies its own territory. Sometime during the last decade, the brand founded by a wide-eyed Hans Wilsdorf in 1905 has transmogrified. It is no longer a watch brand, but rather a phenomenon. The rules do not apply to this relentless juggernaut as it rolls over all-comers. No, when it comes to playing the game, Rolex has a distinct, insurmountable advantage: It wrote the rule book.So rolex
+          <Col>There can be no watch house less in need of an introduction than Rolex. It is a brand whose name echoes beyond the boundaries of our industry. Synonymous with luxury, interchangeable with quality, imbued with a heritage that no amount of money, spin, or PR stunts can synthesize, Rolex almost occupies its own territory. Sometime during the last decade, the brand founded by a wide-eyed Hans Wilsdorf in 1905 has transmogrified. It is no longer a watch brand, but rather a phenomenon. The rules do not apply to this relentless juggernaut as it rolls over all-comers. No, when it comes to playing the game, Rolex has a distinct, insurmountable advantage: It wrote the rule book.So rolex is most exclusuve watch in the world
 
 </Col>
         </Row>
